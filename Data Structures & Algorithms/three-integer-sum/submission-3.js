6c5ed57300fca73bf17bbb9088ad7bd414e8b438
agupta006 <gupta05497@yahoo.in>
2026-05-17 12:44:23 +0530

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const sortedNum = nums.sort((a,b) => a - b);
+        const result = [];
+        const resultSet = new Set();
+        // [-4, -1, -1, 0, 1,2];
+        // const result = [];
+        for(let index = 0; index < sortedNum.length; index++) {
+            const element = sortedNum[index];
+            if (index > 0 &&  element === sortedNum[index - 1]) {
+                continue;
+            }
+            
+            let left = index + 1, right = sortedNum.length - 1;
+            while (left < right) {
+                const threeSum = element + sortedNum[left] + sortedNum[right];
+
+                if (threeSum > 0) {
+                    right--;
+                } else if(threeSum < 0) {
+                    left++;
+                } else {
+                    result.push([element,sortedNum[left], sortedNum[right]]);
+                    left++;
+                    while(sortedNum[left] === sortedNum[left - 1] && left < right) {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return result;
+
+    }
+}
